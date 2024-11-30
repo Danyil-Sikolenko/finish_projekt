99771745d1c9166e63046d58b50c0f5af54070b4
Danyil-Sikolenko <banielsikolenko@gmail.com>
2024-11-30 21:28:05 +0100

    test_works

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 
-// $('.owl-carousel').owlCarousel({
-//     autoplay:true,
-//     autoplayTimeout: 6500,
-//     autoplaySpeed: 1000,
-//     loop: true,
-//     nav: true,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         1100:{
-//             items:1
-//         }
-//     }
+$('.owl-carousel').owlCarousel({
+  autoplay:true,
+  autoplayTimeout: 6500,
+  autoplaySpeed: 1000,
+  loop: true,
+  nav: true,
+  responsive:{
+      0:{
+          items:1
+      },
+      1100:{
+          items:1
+      }
+  }
 
-// })
-
+})
 
 
 
